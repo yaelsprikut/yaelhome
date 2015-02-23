4ba8271837996e8001019475829842863df3ce4b
Yael L Sprikut <luck_be_lady@icloud.com>
2015-02-22 22:04:47 -0500

    Added a profile pic

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <img class="img-responsive" src="img/profile.png" alt="">
+                    <img class="img-responsive" src="img/profile.png" alt="" width="300px" height="300px">
                     <div class="intro-text">
                         <span class="name">Yael Sprikut</span>
                         <hr class="star-light">
